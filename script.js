6b4eb667d73033ac34da2ab99ccceae17143b3a1
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2021-12-13 21:50:01 +0530

    Added the responsive navbar for mobile view with drop down menu and smooth scrolling

--- script.js
+++ script.js
@@ -2,23 +2,29 @@
 
 const mobile = document.querySelector(".mobile");
 const navToggle = document.querySelector(".nav-toggle");
-const navClose = document.querySelector('.nav-close');
-const scrollLink = document.querySelectorAll(".scroll-link")
+const navClose = document.querySelector(".nav-close");
+const scrollLink = document.querySelectorAll(".scroll-link");
+const navbarHeader = document.querySelector(".navbar-header");
 
 navToggle.addEventListener("click", () => {
 	mobile.classList.add("mobile-fix");
 	document.querySelector(".nav-links").classList.add("show-links");
+	navbarHeader.classList.remove("hidden");
+	navToggle.classList.add("hidden");
 });
 
-navClose.addEventListener('click', () => {
+navClose.addEventListener("click", () => {
 	mobile.classList.remove("mobile-fix");
 	document.querySelector(".nav-links").classList.remove("show-links");
-})
-
-scrollLink.forEach(link => {link.addEventListener('click', (e)=>{
-	e.preventDefault();
-	mobile.classList.remove("mobile-fix");
-	document.querySelector(".nav-links").classList.remove("show-links");
-})
+	navbarHeader.classList.add("hidden");
+	navToggle.classList.remove("hidden");
 });
 
+scrollLink.forEach((link) => {
+	link.addEventListener("click", () => {
+		mobile.classList.remove("mobile-fix");
+		document.querySelector(".nav-links").classList.remove("show-links");
+		navbarHeader.classList.add("hidden");
+		navToggle.classList.remove("hidden");
+	});
+});
