1c5c670a3d7828bf5e5ae9e0030efe7cae3b5f52
banlonjones <banlonjones@go-groups.net>
2021-12-13 15:36:52 +0100

    when user click option menu disapear

--- script.js
+++ script.js
@@ -2,8 +2,23 @@
 
 const mobile = document.querySelector(".mobile");
 const navToggle = document.querySelector(".nav-toggle");
+const navClose = document.querySelector('.nav-close');
+const scrollLink = document.querySelectorAll(".scroll-link")
 
 navToggle.addEventListener("click", () => {
-	mobile.classList.toggle("mobile-fix");
+	mobile.classList.add("mobile-fix");
 	document.querySelector(".nav-links").classList.add("show-links");
 });
+
+navClose.addEventListener('click', () => {
+	mobile.classList.remove("mobile-fix");
+	document.querySelector(".nav-links").classList.remove("show-links");
+})
+
+scrollLink.forEach(link => {link.addEventListener('click', (e)=>{
+	e.preventDefault();
+	mobile.classList.remove("mobile-fix");
+	document.querySelector(".nav-links").classList.remove("show-links");
+})
+});
+
